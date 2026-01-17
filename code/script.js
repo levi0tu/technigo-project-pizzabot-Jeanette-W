93260f163ae5266f1a97ea695dc661ef46120a51
Jeanette Wiktorsson <leiia@hotmail.com>
2026-01-17 17:48:05 +0100

    Steg 1 och 2

--- code/script.js
+++ code/script.js
@@ -5,12 +5,19 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
-
+const name = prompt(`What is your name?`)
+alert(`Hi ${name}, I will take your order now. Let´s begin.`)
 // Step 2 - Food choice
 // Your code goes here
-
+const food = prompt(`What will be your choice of food? Answer with a number.
+  1. Pizza
+  2. Pasta
+  3. Salad`
+)
+alert(`You picked ${food}, Great choice. Now what type of ${food} would you like?`)
 // Step 3 - Subtype choice
 // Your code goes here
+//If Pizza
 
 // Step 4 - Age
 // Your code goes here
